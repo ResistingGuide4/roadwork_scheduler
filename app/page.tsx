214440d73fbf2217831c3daa6130494864289bd8
ResistingGuide4 <thomasmrose11@gmail.com>
2024-10-08 14:29:56 -0400

    Style and add more info to homepage

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
-import previewImg from '../public/editorPreview.png'
+import previewImg from '../public/editorPreview.png';
+import style from './homepage.module.css';
 
 export default function Home() {
   return (
@@ -8,9 +9,20 @@ export default function Home() {
         <h1>Roadwork Scheduler</h1>
       </header>
       <main>
-        <div>
-          <Image src={previewImg} width={500} alt="image of editor" />
-          <h2>Quickly and Effectively Schedule Roadwork</h2>
+        <div className={style.imgAndTextGroup}>
+          <Image src={previewImg} width={600} className={style.img} alt="image of editor" />
+          <div className={style.textGroup}>
+            <h2>Quickly and Effectively Schedule Roadwork</h2>
+            <p>Using data from google maps and input from the user, Roadwork Scheduler can arrange and schedule optimal times to work on and close down roads.</p>
+          </div>
+        </div>
+        <div className={style.imgAndTextGroup}>
+          <div className={style.textGroup}>
+            <h2>Avoid Problems in Traffic</h2>
+            <p>Roadwork scheduler will take into account buildings of interest such as Schools, Colleges, Fire departments, and other importent buildings, and prevent overusing roads for detours.
+            </p>
+          </div>
+          <Image src={previewImg} className={style.img} width={600} alt="image of editor" />
         </div>
       </main>
     </>
